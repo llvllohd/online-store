ea69cf355f35b84724aafbf5543b5d69c2668d4e
llvllohd <llvllohd@gmail.com>
2021-05-22 23:19:33 +0530

    deploying

--- src/components/HomeScreen.vue
+++ src/components/HomeScreen.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center justify-center sidebar-custom-height">
-    <div class="w-1/3 h-1/3 bg-red-400 flex items-center justify-center">
+  <div class="flex items-center justify-center">
+    <div class="w-1/3 h-1/3 bg-red-400 flex items-center justify-center mt-2">
       <h3>I am Home</h3>
     </div>
   </div>
--- src/components/SettingsScreen.vue
+++ src/components/SettingsScreen.vue
@@ -1,6 +1,6 @@
 <template>
-  <div class="flex items-center justify-center sidebar-custom-height">
-    <div class="w-1/3 h-1/3 bg-gray-400 flex items-center justify-center">
+  <div class="flex items-center justify-center">
+    <div class="w-1/3 bg-gray-400 flex items-center justify-center mt-1">
       <h3>I am Settings</h3>
     </div>
   </div>
@@ -8,7 +8,7 @@
 
 <style scoped>
 .sidebar-custom-height {
-  height: calc(100vh - theme("spacing.16"));
+  height: calc(100vh - theme("spacing.48"));
   transition: all 0.5s ease-in-out;
 }
 </style>
